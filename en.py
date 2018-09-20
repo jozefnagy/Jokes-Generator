e248dab2a8fc062e780f9d4eb69af112cdf42642
Jozef Nagy <43299588+jozefnagy@users.noreply.github.com>
2018-09-20 10:29:35 +0200

    NOT COMPLETED YET

---
+++ en.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+import sys
+import random
+
+vtipy = ["Some people say that leaving your wife is hard. Some people say that it's impossible.", "When I see chocolate, I hear two voices. First says: 'You need to eat that chocolate.' the second says: 'You heard it. Eat it.' ", "My wife cooks that bad, that we praise the god before AND after eating.", "Proè není svatba dvou zadnicových pùlek dobrý nápad? Protože jdou od sebe kvùli každému prdu.",
+         "Rád bych zakoupil nový bumerang. A taky bych potøeboval poradit, jak vyhodit ten starý.", "Vìda nám dala letadla, nebetyèné mrakodrapy… Ale jen náboženství dokáže tyhle dvì vìci spojit.", "Mùj nedostatek elektrikáøských znalostí pro mnoho lidí èasto pøedstavuje šok..", "Kdo je sakra Rorschach, a proè namaloval tolik obrázkù mojí matky?!", "Co dostanete, když zkøížíte øeènickou otázku s vtipem?", "Øíká se, že za prachy si štìstí nekoupíš. Ale každá teorie by se mìla otestovat."]
+
+
+def main():
+    print("Jokes Generator --- Jozef Nagy v. 1.0\n")
+    print("-------------------------------------\n\n")
+
+    # 1 is the smallest number of number generation (Don't change), Second number change only if you add joke (1 new joke = +1)(4th line))
+    print(jokes[random.randint(1, 10)])
+
+
+main()
